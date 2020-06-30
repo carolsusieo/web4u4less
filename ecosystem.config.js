6ae757c35d877d2cf309e77729dc441cf6d0281e
Carol Odiorne <carolsusieo@gmail.com>
2020-06-29 21:12:48 -0500

    Add pm2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+apps : [
+     {
+       name          : 'web4u4less',
+       script        : 'npx',
+       interpreter   : 'none',
+       args          : 'serve build -s',
+       env_production : {
+         NODE_ENV: 'production'
+       }
+     }
+   ]
